bce61f8cbb34bd0f201e27442e9d4ad5799a6457
Sebastian Skałacki <skalee@gmail.com>
2018-08-08 07:45:50 +0200

    Gem bootstrapped with Bundler
    Make new gem on top of files extracted from EnMail, master as of ref
    https://github.com/riboseinc/enmail/commit/9142dd7b899414bd47a4fb7.

---
+++ spec/rspec/pgp_matchers_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Rspec::PgpMatchers do
+  it "has a version number" do
+    expect(Rspec::PgpMatchers::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
