4ab94bef5c798bb9f05c26d41a06576802fc5b95
Sebastian Skałacki <skalee@gmail.com>
2018-08-18 20:20:45 +0200

    Fix module spelling
    Use capital letters where appropriate.

--- spec/rspec/pgp_matchers_spec.rb
+++ spec/rspec/pgp_matchers_spec.rb
@@ -1,8 +1,8 @@
 # (c) Copyright 2018 Ribose Inc.
 #
 
-RSpec.describe Rspec::PgpMatchers do
+RSpec.describe RSpec::PGPMatchers do
   it "has a version number" do
-    expect(Rspec::PgpMatchers::VERSION).not_to be nil
+    expect(RSpec::PGPMatchers::VERSION).not_to be nil
   end
 end
