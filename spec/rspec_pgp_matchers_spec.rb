58f00e1324a3793040687422bddfd9459d544098
Sebastian Skałacki <skalee@gmail.com>
2018-08-23 16:13:52 +0200

    Test that PGP home directory attribute is settable

--- spec/rspec_pgp_matchers_spec.rb
+++ spec/rspec_pgp_matchers_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe RSpec::PGPMatchers do
   it "has a version number" do
     expect(RSpec::PGPMatchers::VERSION).not_to be nil
   end
+
+  # This test would break others if run in parallel
+  it "has configurable homedir" do
+    homedir_preserve = RSpec::PGPMatchers.homedir
+    expect { RSpec::PGPMatchers.homedir = "some/path" }.
+      to change { RSpec::PGPMatchers.homedir }.to("some/path")
+    RSpec::PGPMatchers.homedir = homedir_preserve
+  end
 end
