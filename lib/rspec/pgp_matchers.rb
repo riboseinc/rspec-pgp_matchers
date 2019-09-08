161a3e70c64a6778a4d9340dfdd2d99c303250c0
Sebastian Skałacki <skalee@gmail.com>
2019-09-08 06:58:34 +0200

    Improve API documentation for this gem

--- lib/rspec/pgp_matchers.rb
+++ lib/rspec/pgp_matchers.rb
@@ -12,7 +12,30 @@ module RSpec
     @gpg_executable = "gpg"
 
     class << self
+      # Name of the GnuPG executable or path to that executable.  Defaults to
+      # +gpg+.
+      #
+      # Absolute and relative paths are allowed, but usually setting +$PATH+
+      # environment variable is a better idea.
+      #
+      # @return [String] executable name or absolute or relative path to that
+      #   executable
       attr_accessor :gpg_executable
+
+      # Path to the OpenPGP home directory.  Defaults to +nil+ and must be set
+      # prior using the matchers.
+      #
+      # Given directory may be initialized with other tool than GnuPG, e.g. RNP,
+      # but it must be in a format which is readable by GnuPG.  Also,
+      # if specified directory is empty, then it will be initialized by GnuPG
+      # at first use, it must exist though.  Nevertheless, the latter case is
+      # not very practical, as the OpenPGP home directory created this way
+      # contains no keys.
+      #
+      # It is recommended to have a dedicated PGP home directory just for
+      # testing, so that test keys are separated from regular ones.
+      #
+      # @return [String] absolute or relative path to the OpenPGP home directory
       attr_accessor :homedir
     end
   end
